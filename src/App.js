aa5c49e3df4b2fdbf54335c49a13d9c575604fa1
JurgenJGP <yurgen6661@gmail.com>
2023-11-22 09:09:34 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -16,12 +16,23 @@ export default function App() {
       <section id="hero">
         <div className="containers pt-[94px] pb-[54px] grid gap-y-5 md:grid-cols-2 items-center">
           <div className=" ">
-            <h1 className="text-[#094D10] font-[Sora] font-bold text-center text-[45px] leading-tight">Valoramos la agricultura orgánica y sus historias</h1>
-            <p className="text-[24px] text-center mt-5 font-[Mulish] font-semibold">Vive la experiencia saludable y orgánica de la palta arequipeña, y te obsequiamos xxx por tu primera
+            <h1 
+              className="text-[#094D10] font-[Sora] font-bold text-center text-[45px] leading-tight
+              md:text-left">
+              Valoramos la agricultura orgánica y sus historias
+            </h1>
+            <p 
+              className="text-[24px] text-center  mt-5 font-[Mulish] font-semibold
+              md:text-left">
+              Vive la experiencia saludable y orgánica de la palta arequipeña, y te obsequiamos xxx por tu primera
               compra.
             </p>
             <div className="flex justify-center md:justify-normal">
-              <button className="bg-[#89C241] md:bg-[#F64B25] rounded-[25px] px-20 mt-8 py-4 text-[20px] font-[Inter] font-bold text-white">Consultar pedido</button>
+              <button 
+                className="bg-[#89C241]  rounded-[25px] px-10 sm:px-20 mt-8 py-4 text-[20px] font-[Inter] font-bold text-white
+                md:bg-[#F64B25]"> 
+                Consultar pedido
+              </button>
             </div>
           </div>
           <img src="image/hero.png" alt="" className="w-auto" />
@@ -33,7 +44,9 @@ export default function App() {
             <h2>
               ¿Qué nos motiva cada día?
             </h2>
-            <div className="grid gap-y-[180px] sm:grid-cols-2 lg:grid-cols-3 md:gap-x-4 md:gap-y-[150px] lg:gap-[96px] mb-[131px]">
+            <div 
+              className="grid gap-y-[180px] 
+              sm:grid-cols-2 sm:gap-x-4 md:gap-y-[150px] lg:grid-cols-3 lg:gap-[96px] mb-[131px]">
               <div className="flex justify-center relative col-auto ">
                 <img src="/image/motiva/motiva1.jpg" alt="" className="rounded-[20px] max-w-[500px] w-full" />
                 <div className="cards">
@@ -70,19 +83,28 @@ export default function App() {
             </div>
           </div>
           <div className="flex justify-center">
-            <button className="bg-[#F64B25] rounded-[25px] px-[40px] py-[14px] text-[20px] text-white mt-[32px] md:mt-[48px]">Consulta tu pedido</button>
+            <button 
+              className="bg-[#F64B25] rounded-[25px] px-[40px] py-[14px] text-[20px] text-white mt-[32px] md:mt-[48px]">
+              Consulta tu pedido
+            </button>
           </div>
         </div>
       </section>
       <section id="inicios">
-        <div className="containers grid md:grid-cols-2 gap-[88px] pt-[40px] md:pt-[107px]">
+        <div className="containers grid gap-[88px] pt-[40px] md:grid-cols-2 md:pt-[107px]">
           <div className="flex justify-center relative  mx-w-[328px]">
-            <img src="/image/inicios/inicios1.png" alt="" className="w-[280px] md:w-[580px] md:h-[512px] rounded-lg" />
-            <img src="/image/inicios/inicios2.png" alt="" className="absolute top-[346px] -mt-[230px] ml-[133px] w-[211px] md:w-[440px] md:translate-x-[50%] md:right-[50%] md:-mt-[15%] rounded-lg" />
+            <img src="/image/inicios/inicios1.png" alt="" className="w-[80%] md:h-[512px] rounded-lg" />
+            <img src="/image/inicios/inicios2.png" alt="" 
+              className="absolute top-[40%] w-[60%] rounded-lg" />
           </div>
           <div className=" lg:mb-[200px] relative">
-            <h2 className="text-[#094D10] font-bold text-center md:text-left">Nuestros inicios</h2>
-            <p className="text-[16px] leading-9 pt-[18px] text-center md:text-left">Los hermanos Hipo y Pancho Cáceres, empezaron su aventura de <br></br> producción de paltas por el 2019, en su tierra natal, Tomepampa <br></br>
+            <h2 
+              className="text-[#094D10] font-bold text-center md:text-left">
+              Nuestros inicios
+            </h2>
+            <p 
+              className="text-[16px] pt-[18px] px-4 text-center md:text-left md:leading-9">
+              Los hermanos Hipo y Pancho Cáceres, empezaron su aventura de <br></br> producción de paltas por el 2019, en su tierra natal, Tomepampa <br></br>
               Arequipa y siendo ellos muy autodidactas y emprendedores, decidieron aprender cómo lograr una producción más orgánica, viendo videos de YouTube, con lápiz y cuaderno en mano tomando buenas prácticas de otros países y consultando a otros amig@s productores.
               <br></br>
               <br></br>
@@ -90,42 +112,58 @@ export default function App() {
               Desde entonces, cada año, esperan con ansias la nueva cosecha, a base de esfuerzo y cariño para lograr una producción que le devuelva el valor a la biodiversidad de su tierra.Te invitamos a descubrir más de sus aventuras aquí.
             </p>
             <div className="flex justify-center">
-              <button className="bg-[#F64B25] mb-9 rounded-[25px] px-10 ms:px-20 mt-[52px] py-3.5 text-[20px] text-white">Lee nuestras historias</button>
+              <button 
+                className="bg-[#F64B25] mb-9 rounded-[25px] px-10 ms:px-20 mt-[52px] py-3.5 text-[20px] text-white">
+                Lee nuestras historias
+              </button>
             </div>
           </div>
         </div>
       </section>
       <section id="testimonios" className="bg-green-200">
-        <div className="containers py-[40px] md:py-[72px]">
-          <h2 className="text-center">
+        <div className="containers grid justify-center py-[40px] md:py-[72px]">
+          <h2 
+            className="text-center">
             Ellos ya probaron nuestros productos
           </h2>
-          <div className="grid gap-8 justify-center md:grid-cols-3 lg:gap-[182px] lg:px-[80px]">
-            <div className="bg-white flex flex-col justify-center w-[220px] h-[383px] rounded-tl-[110px] rounded-tr-[110px] gap-[23px] px-[11.5px] ">
+          <div className="grid gap-8 sm:grid-cols-2 sm:gap-x-4 md:grid-cols-3 lg:gap-[182px] lg:px-[80px]">
+            <div className="cardtest ">
               <div className=" flex justify-center items-center">
                 <img src="/image/testimonios/testimonio1.png" alt="" className="rounded-lg w-[160px]" />
               </div>
               <div>
-                <p className="text-center">Me ayudaron a  complementar mi dieta con productos de calidad y alimentación saludable.</p>
-                <p className="text-center mt-[8px]">Martita, 25 años</p>
+                <p className="text-center">
+                  Me ayudaron a  complementar mi dieta con productos de calidad y alimentación saludable.
+                </p>
+                <p className="text-center mt-[8px]">
+                  Martita, 25 años
+                </p>
               </div>
             </div>
-            <div className="bg-white flex flex-col justify-center w-[220px] h-[383px] rounded-tl-[110px] rounded-tr-[110px] gap-[23px] px-[11.5px] ">
+            <div className="cardtest">
               <div className=" flex justify-center items-center">
                 <img src="/image/testimonios/testimonio2.png" alt="" className="rounded-lg w-[160px]" />
               </div>
               <div>
-                <p className="text-center">Me ayudaron a  complementar mi dieta con productos de calidad y alimentación saludable.</p>
-                <p className="text-center mt-[8px]">Martita, 25 años</p>
+                <p className="text-center">
+                  Me ayudaron a  complementar mi dieta con productos de calidad y alimentación saludable.
+                </p>
+                <p className="text-center mt-[8px]">
+                  Martita, 25 años
+                </p>
               </div>
             </div>
-            <div className="bg-white flex flex-col justify-center w-[220px] h-[383px] rounded-tl-[110px] rounded-tr-[110px] gap-[23px] px-[11.5px] ">
+            <div className="cardtest ">
               <div className=" flex justify-center items-center">
                 <img src="/image/testimonios/testimonio3.png" alt="" className="rounded-lg w-[160px]" />
               </div>
               <div>
-                <p className="text-center">Me ayudaron a  complementar mi dieta con productos de calidad y alimentación saludable.</p>
-                <p className="text-center mt-[8px]">Martita, 25 años</p>
+                <p className="text-center">
+                  Me ayudaron a  complementar mi dieta con productos de calidad y alimentación saludable.
+                </p>
+                <p className="text-center mt-[8px]">
+                  Martita, 25 años
+                </p>
               </div>
             </div>
           </div>
@@ -141,11 +179,15 @@ export default function App() {
                 <h2 className="text-center text-white ">
                   ¿Te gustaría colaborar con nosotros?
                 </h2>
-                <p className="text-center text-white">Envíanos un mensaje y con gusto conversamos</p>
+                <p className="text-center text-white">
+                  Envíanos un mensaje y con gusto conversamos
+                </p>
               </div>
               <div className="my-[32px] z-30">
-                <h4 className="text-center text-[#FF9A2E] mb-[32px]">nutrip@gmail.com</h4>
-                <div className="flex justify-center items-center gap-[32px]">
+                <h4 className="text-center text-[#FF9A2E] mb-[32px]">
+                  nutrip@gmail.com
+                </h4>
+                <div className="flex justify-center items-center gap-4 sm:gap-[32px]">
                   <img src="/image/redessociales/facebook.svg" alt="" />
                   <img src="/image/redessociales/youtube.svg" alt="" />
                   <img src="/image/redessociales/insta.svg" alt="" />
@@ -154,7 +196,9 @@ export default function App() {
                 </div>
               </div>
               <img src="/image/logofooter.svg" alt="z-30" />
-              <p className="text-center text-white z-30">Todos los derechos reservados © NUTRIP | Design by CICLOS STUDIO</p>
+              <p className="text-center text-white z-30">
+                Todos los derechos reservados © NUTRIP | Design by CICLOS STUDIO
+              </p>
             </div>
           </div>
         </div>
